6eb35f7613b04bb63dbec6bafe38af6eb17f9876
ccckmit <ccckmit@gmail.com>
2024-10-25 09:42:35 +0800

    add wshello/js

--- 02-後端/09-websocket/00-wshello/js/server.js
+++ 02-後端/09-websocket/00-wshello/js/server.js
@@ -1,16 +1,11 @@
 import { WebSocketServer } from "https://deno.land/x/websocket@v0.1.3/mod.ts";
 
-const clientMap = new Map()
-const wss = new WebSocketServer(8080)
+const wss = new WebSocketServer(8080);
 console.log('server started at 8080 port')
 wss.on("connection", function (ws) {
-  clientMap.set(ws, {})
+  console.log('some client connected...')
   ws.on("message", function (message) {
     console.log(message);
-    for (let [client, obj] of clientMap) {
-      if (client != ws) {
-        client.send(message)
-      }
-    }
-  })
-})
+    ws.send(message);
+  });
+});
