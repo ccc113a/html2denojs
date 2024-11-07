4dcee5428aa981384cbba76bda08f6a24a220260
ccckmit <ccckmit@gmail.com>
2024-11-07 16:10:42 +0800

    multipart fixed

--- 02-後端/10-file/01-multipart/fileupload.js
+++ 02-後端/10-file/01-multipart/fileupload.js
@@ -30,5 +30,5 @@ app.use(oakCors()); // Enable CORS for All Routes
 app.use(router.routes())
 app.use(router.allowedMethods())
 
-console.log('Server run at http://127.0.0.1:3001')
-await app.listen({ port: 3001 })
+console.log('Server run at http://127.0.0.1:6789')
+await app.listen({ port: 6789 })
